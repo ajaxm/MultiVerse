dbeb2bd84059f3a675f1e3401157266963fded32
Adithya Manohar <adi4778@gmail.com>
2015-10-13 10:48:54 -0700

    create user and session controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username], params[:user][:password]
+    )
+    if @user
+      log_in(@user)
+      # redirect to poems index
+      render json: "logged in!", status: 200
+    else
+      flash.now[:msg] = ["Invalid login credentials."]
+      @user = User.new(username: params[:user][:username])
+      render :new
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to new_session_url
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      log_in(@user)
+      render json: "user created!", status: 200
+    else
+      flash.now[:msg] = @user.errors.full_messages,
+                        status: :unprocessible_entity
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
