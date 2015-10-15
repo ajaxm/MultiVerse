a0634282e042fa139afe70e7e2bf56a16cdb9e87
Adithya Manohar <adi4778@gmail.com>
2015-10-14 17:07:03 -0700

    set up React Router for poems index

--- app/assets/javascripts/multiverse.js.jsx
+++ app/assets/javascripts/multiverse.js.jsx
@@ -1,3 +1,27 @@
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>The MultiVerse</h1>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+var routes = (
+  <Route path='/' component={App}>
+    <IndexRoute component={Index}/>
+    <Route path='poems/:poemId' component={Poem}/>
+  </Route>
+);
+
 $(function() {
-  React.render(<Index/>, document.getElementById('multiverse-container'));
+  React.render(<Router>{routes}</Router>,
+    document.getElementById('multiverse-container')
+  );
 });
