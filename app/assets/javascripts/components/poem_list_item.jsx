1bdb6421a807d97f1bf9a98afbf5acb7f2bbd9ec
Adithya Manohar <adi4778@gmail.com>
2015-10-19 18:12:16 -0700

    distinguish home poems by contribution status

--- app/assets/javascripts/components/poem_list_item.jsx
+++ app/assets/javascripts/components/poem_list_item.jsx
@@ -1,17 +1,30 @@
 var PoemListItem = React.createClass({
   render: function() {
     var poem = this.props;
-    var poemPreview, poemLink;
+
+    var poemPreview, poemLink, contributionStatus;
+
     if (poem.status === 'complete') {
       poemPreview = poem.first_stanza;
       poemLink = '/#archive/';
+      contributionStatus = 'complete';
     } else {
-      poemPreview = poem.last_line; ///
+      poemPreview = poem.last_line;
       poemLink = '/#poems/';
+      if (poem.involves_current_user) {
+        if (poem.last_author_id === window.currentUserId) {
+          contributionStatus = 'last-contribution';
+        } else {
+        contributionStatus = 'contributed';
+        }
+      } else {
+        contributionStatus = 'not-contributed';
+      }
     }
+
     return (
       <a href={poemLink + poem.id}>
-        <li>
+        <li className={contributionStatus}>
           <div className='list-item-title'>{poem.title}</div>
           {poemPreview}
         </li>
