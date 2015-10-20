69b86e78e47a4b1f328d899662c29018fca01cad
Adithya Manohar <adi4778@gmail.com>
2015-10-20 09:38:55 -0700

    save double-scroll home page for possible later use

---
+++ app/assets/javascripts/components/double_scroll.js.jsx
@@ -0,0 +1,56 @@
+// var Home = React.createClass({
+//   getInitialState: function() {
+//     return {
+//       contributed: PoemStore.involvingCurrentUser(),
+//       nonContributed: PoemStore.notInvolvingCurrentUser()
+//     };
+//   },
+//
+//   componentDidMount: function() {
+//     PoemStore.addChangeListener(this._onChangeEvent);
+//     ApiUtil.fetchPoems({'status': 'incomplete'});
+//   },
+//
+//   componentWillUnmount: function() {
+//     PoemStore.removeChangeListener(this._onChangeEvent);
+//   },
+//
+//   _onChangeEvent: function() {
+//     this.setState({
+//       contributed: PoemStore.involvingCurrentUser(),
+//       nonContributed: PoemStore.notInvolvingCurrentUser()
+//     });
+//   },
+//
+//   _getUserPoems: function() {
+//     var that = this;
+//     var userPoems = this.state.contributed.map(function(poem){
+//       return (
+//         <PoemListItem status='incomplete' key={poem.id} {...poem}/>
+//       );
+//     });
+//     return userPoems;
+//   },
+//
+//   _getNonUserPoems: function() {
+//     var nonUserPoems = this.state.nonContributed.map(function(poem){
+//       return (
+//         <PoemListItem status='incomplete' key={poem.id} {...poem}/>
+//       );
+//     });
+//     return nonUserPoems;
+//   },
+//
+//   render: function() {
+//     var userPoems = this._getUserPoems();
+//     var nonUserPoems = this._getNonUserPoems();
+//     return(
+//       <div className='home-container'>
+//         <ul className='poem-index'>{nonUserPoems}</ul>
+//         <br/>
+//         <div className='contributions-label'>You've contributed to:</div>
+//         <ul className='poem-index'>{userPoems}</ul>
+//       </div>
+//     );
+//   }
+// });
