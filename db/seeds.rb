981e5ba0ee9fc4e8afa5cdf51ce6ab785938cd11
Adithya Manohar <adi4778@gmail.com>
2015-10-20 18:22:01 -0700

    fix actual problem: linebreaks in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,21 +36,21 @@ terrancehayes = User.create(
 summerday = Poem.create(
   title: 'The Summer Day',
   author: maryoliver,
-  first_stanza: "Who made the world? \n
+  first_stanza: "Who made the world?
     Who made the swan, and the black bear?",
   num_stanzas: 6
 )
 aimlesslove = Poem.create(
   title: 'Aimless Love',
   author: billycollins,
-  first_stanza: "This morning as I walked along the lakeshore, \n
+  first_stanza: "This morning as I walked along the lakeshore,
     I fell in love with a wren"
 )
 boxcar = Poem.create(
   title: 'Boxcar',
   author: terrancehayes,
-  first_stanza: "Black as snow & ice as cool \n
-    Miles stood horn-handed while \n
+  first_stanza: "Black as snow & ice as cool
+    Miles stood horn-handed while
     John so&soloed",
   num_stanzas: 8
 )
@@ -60,36 +60,36 @@ sleep(1)
 ##########
 
 Stanza.create([
-  { body: "The love of the chestnut, \n
+  { body: "The love of the chestnut,
       the jazz cap and one hand on the wheel.",
     author: billycollins,
     poem: summerday,
     order: 2
   },
-  { body: "I don't know exactly what a prayer is. \n
-      I do know how to pay attention, how to fall down \n
+  { body: "I don't know exactly what a prayer is.
+      I do know how to pay attention, how to fall down
       into the grass, how to kneel down in the grass,",
     author: maryoliver,
     poem: summerday,
     order: 3
   },
-  { body: "but my heart is always propped up \n
-      in a field on its tripod, \n
+  { body: "but my heart is always propped up
+      in a field on its tripod,
       ready for the next arrow.",
     author: billycollins,
     poem: summerday,
     order: 4
   },
   {
-    body: "And the days were bright red, \n
-      and every time we kissed there was \n
+    body: "And the days were bright red,
+      and every time we kissed there was
       another apple to slice into pieces.",
     author: richardsiken,
     poem: summerday,
     order: 5
   },
   {
-    body: "Tell me, what is it you plan to do \n
+    body: "Tell me, what is it you plan to do
       with your one wild and precious life?",
     author: maryoliver,
     poem: summerday,
@@ -98,14 +98,14 @@ Stanza.create([
   ])
 
   Stanza.create([
-  { body: "or the owl arranging all of the night, \n
-      which is his wisdom, or the poem \n
+  { body: "or the owl arranging all of the night,
+      which is his wisdom, or the poem
       filling your pillow with its blue feathers.",
     author: markstrand,
     poem: aimlesslove,
     order: 2
   },
-  { body: "Tell me, what else should I have done? \n
+  { body: "Tell me, what else should I have done?
       Doesn't everything die at last, and too soon?",
     author: maryoliver,
     poem: aimlesslove,
