9f00768cddf5a014a6212537eb3d9fa9c01d5774
Adithya Manohar <adi4778@gmail.com>
2015-10-25 17:06:41 -0700

    white navbar styling

--- app/assets/javascripts/components/sidebar.js.jsx
+++ app/assets/javascripts/components/sidebar.js.jsx
@@ -7,10 +7,10 @@ var Sidebar = React.createClass({
     return (
       <div className='sidebar-container'>
         <span className='sidebar-username'>{window.currentUserName}</span>
-        <a className='sidebar-button' href='/#about'>About</a>
-        <a className='sidebar-button' href='/#new'>New</a>
+        <a className='sidebar-button' href='/#about'>The MultiVerse</a>
         <a className='sidebar-button' href='/#'>Home</a>
         <a className='sidebar-button' href='/#archive'>Archive</a>
+        <a className='sidebar-button' href='/#new'>New</a>
         <a className='sidebar-button'
            onClick={this.handleLogOut}
            href='#'>Log Out</a>
