1b57aaa1914ae58ee86a2772d6ac65c7da9c1ee3
Adithya Manohar <adi4778@gmail.com>
2015-10-26 20:44:28 -0700

    style about page

--- app/assets/javascripts/components/about.js.jsx
+++ app/assets/javascripts/components/about.js.jsx
@@ -1,22 +1,37 @@
 var About = React.createClass({
   render: function() {
+    var githubLink = (
+      <a href='http://github.com/Ajax4778/MultiVerse'>
+        For the Github page, click here.
+      </a>
+    );
+    var wikiLink = (
+      <a href='http://en.wikipedia.org/wiki/Exquisite_corpse'>
+        "Exquisite Corpse"
+      </a>
+    );
     return (
       <div className='about-container'>
-        <h2 className='about-header'>About The MultiVerse</h2>
-        <ul>
-          <li>The MultiVerse is a collaborative poetry-writing application.</li>
-          <li>As a user of the MultiVerse, you can create new poems and
-            contribute stanzas to existing ones.</li>
-          <li>When you create a poem, you specify the number of stanzas it needs
-            to have, and write the first one. When it reaches that number, it is
-            closed for contribution and added to the archive.</li>
-          <li>When a poem is in progress, only its last line is visibile. The
-            next stanza must be written based on this line. When the poem is
-            completed, it becomes fully visible.</li>
+        <div className="lines"></div>
+        <h2 className='about-header'>The MultiVerse</h2>
+        <ul className='about-list'>
+          <li>The MultiVerse is a collaborative poetry-writing application
+            following the {wikiLink} method.</li>
+          <li className='about-subheader'>Creating Poems:</li>
+          <li>On creating a poem, you write the first stanza and specify the
+            number of stanzas the poem should have. When it reaches that length,
+            it is closed for contribution and added to the archive.</li>
+          <li className='about-subheader'>Contributing Stanzas:</li>
+          <li>Before a poem is completed, only its most recent line is visible.
+            The next stanza is written based on this line.</li>
           <li>You may contribute multiple times to the same poem provided you do
             not write successive stanzas.</li>
-          <li>Stanzas must be at least two lines long, and no more than three
-            lines long.</li>
+          <li>Stanzas must be at least two lines long, and no more than three.
+            (That is, your input must have either one linebreak or two; no more,
+            no fewer.) </li>
+          <li className='about-subheader'>About:</li>
+          <li>The Multiverse was created by Adithya "Ajax" Manohar using
+            React.js and Rails. {githubLink}</li>
         </ul>
       </div>
     );
