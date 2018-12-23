ce8b158dcf96ba92bd3e651fdb884a03071475bc
Ajax Manohar <ajax.manohar@sonder.com>
2018-12-23 13:09:35 -0800

    Upgrade to Rails 5 (#4)

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -21,10 +20,9 @@ ActiveRecord::Schema.define(version: 20151020221309) do
     t.integer  "user_id",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["poem_id"], name: "index_favorites_on_poem_id", using: :btree
+    t.index ["user_id", "poem_id"], name: "index_favorites_on_user_id_and_poem_id", unique: true, using: :btree
   end
-
-  add_index "favorites", ["poem_id"], name: "index_favorites_on_poem_id", using: :btree
-  add_index "favorites", ["user_id", "poem_id"], name: "index_favorites_on_user_id_and_poem_id", unique: true, using: :btree
 
   create_table "poems", force: :cascade do |t|
     t.string   "title",                   null: false
@@ -32,10 +30,9 @@ ActiveRecord::Schema.define(version: 20151020221309) do
     t.integer  "num_stanzas", default: 6, null: false
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.index ["author_id"], name: "index_poems_on_author_id", using: :btree
+    t.index ["title"], name: "index_poems_on_title", using: :btree
   end
-
-  add_index "poems", ["author_id"], name: "index_poems_on_author_id", using: :btree
-  add_index "poems", ["title"], name: "index_poems_on_title", using: :btree
 
   create_table "stanzas", force: :cascade do |t|
     t.text     "body",       null: false
@@ -44,10 +41,9 @@ ActiveRecord::Schema.define(version: 20151020221309) do
     t.integer  "author_id",  null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_stanzas_on_author_id", using: :btree
+    t.index ["poem_id"], name: "index_stanzas_on_poem_id", using: :btree
   end
-
-  add_index "stanzas", ["author_id"], name: "index_stanzas_on_author_id", using: :btree
-  add_index "stanzas", ["poem_id"], name: "index_stanzas_on_poem_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
@@ -55,9 +51,8 @@ ActiveRecord::Schema.define(version: 20151020221309) do
     t.string   "session_token",   null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true, using: :btree
+    t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
-
-  add_index "users", ["session_token"], name: "index_users_on_session_token", unique: true, using: :btree
-  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
